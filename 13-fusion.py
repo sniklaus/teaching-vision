b2f90947ff3c1b87ee7e12b30c71f7456cdbc9eb
sniklaus <simon.niklaus@outlook.com>
2018-02-13 19:41:11 -0800

    no message

--- 13-fusion.py
+++ 13-fusion.py
@@ -11,7 +11,7 @@ numpyInputs = [
 
 # use the quality measures to extract a weight map for each image according to section 3.1
 # set the weighting exponents to one, thus equaling the contrition of contrast, saturation, and exposedness
-# make sure to add a small epsilon to each weight map to avoid divisions by zero in the subsequent step
+# make sure to add 0.0000001 to the weight map of each image to avoid divisions by zero in the subsequent step
 # normalize the weight maps such that they sum up to one at each pixel as described in section 3.2
 # store the three weight maps in the numpyWeights array which will be used below to perform the blending
 
